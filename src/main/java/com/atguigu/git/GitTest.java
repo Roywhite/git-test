101190daf61dedc06ba8215852fb8e7c8bffe3a2
xiaobai <1075519964@qq.com>
2021-07-07 16:42:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,18 @@
+/**
+ * 文件名：GitTest
+ * 版权：Copyright 2017-2022 xiaobai All Rights Reserved.
+ * 描述：
+ */
+
+package com.atguigu.git;
+
+/**
+ * @author xiaobai
+ * @version 1.0
+ * @date 2021/7/7 16:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git！");
+    }
+}
